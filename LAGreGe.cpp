83b89f0f2ace23dbb3011ea67981db97dcc229c2
Harambl <aspentya@gmail.com>
2026-04-16 14:11:43 +1000

    Update LAGreGe.cpp

--- LAGreGe.cpp
+++ LAGreGe.cpp
@@ -5,18 +5,18 @@
 #include <algorithm>
 #include <clocale>
 
-double func(double x) {
+long double func(long double x) {
     return x - sin(x);
 }
 
-double LagranF1(double x_, double xi, double xi_1) {
+long double LagranF1(long double x_, long double xi,long  double xi_1) {
     return func(xi) * (x_ - xi_1) / (xi - xi_1) + func(xi_1) * (x_ - xi) / (xi_1 - xi);
 }
 
-double LagranF2(double x_, double xi, double xi_1, double x1) {
+long double LagranF2(long double x_, long double xi, long double xi_1, long double x1) {
     return func(x1) * ((x_ - xi) * (x_ - xi_1)) / ((x1 - xi) * (x1 - xi_1)) 
-        + func(xi) * ((x_ - x1) * (x_ - xi - 1)) / ((xi - x1) * (xi - xi_1)) 
-        + func(xi_1) * ((x_ - xi_1) * (x_ - xi)) / ((xi_1 - x1) * (xi_1 - xi));
+        + func(xi) * ((x_ - x1) * (x_ - xi_1)) / ((xi - x1) * (xi - xi_1)) 
+        + func(xi_1) * ((x_ - x1) * (x_ - xi)) / ((xi_1 - x1) * (xi_1 - xi));
 }
 
 
@@ -26,21 +26,21 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    double a = 0.6;
-    double b = 1.1;
+    long double a = 0.6;
+    long double b = 1.1;
 
-    vector<double> x_ = {0.88, 0.63, 1.08, 0.83};
+    vector<long double> x_ = { 0.88, 0.63, 1.08, 0.83 };
 
-    double h = (b - a) / 10;
+    long double h = (b - a) / 10;
 
-    double x__ = x_[0];
+    long double x__ = x_[0];
     int i_ = 0;
-    double xi = 0;
+    long double xi = 0;
 
-    vector<double> x (11, 0);
+    vector<long double> x(11, 0);
     for (int i = 0; i < 11; ++i) {
         x[i] = a + i * h;
-        if(x[i] < x__){
+        if (x[i] < x__) {
             if (x[i] > xi) {
                 xi = x[i];
                 i_ = i;
@@ -50,15 +50,79 @@ int main() {
 
     cout << "Таблица x_i" << endl;
     int count = 1;
-    for (double xi : x) {
+    for (long double xi : x) {
         cout << "x[" << count << "]= " << xi << endl;
         count++;
     }
 
-    for (double x : x_) {
+    cout << endl;
 
+    {
+    cout << "При x_ = " << x__ << endl;
+    cout << "L1= " << LagranF1(x__, xi, x[i_ + 1]) << endl;
+    cout << "L2= " << LagranF2(x__, xi, x[i_ + 1], x[i_ - 1]) << endl;
+    cout << endl;
     }
 
+    x__ = x_[1];
+    i_ = 0;
+    xi = 0;
+
+    for (int i = 0; i < 11; ++i) {
+        if (x[i] < x__) {
+            if (x[i] > xi) {
+                xi = x[i];
+                i_ = i;
+            }
+        }
+    }
+
+    {
+        cout << "При x_ = " << x__ << endl;
+        cout << "L1= " << LagranF1(x__, xi, x[i_ + 1]) << endl;
+        cout << "L2= " << LagranF2(x__, xi, x[i_ + 1], x[i_ + 2]) << endl;
+        cout << endl;
+    }
+
+    x__ = x_[2];
+    i_ = 0;
+    xi = 0;
+
+    for (int i = 0; i < 11; ++i) {
+        if (x[i] < x__) {
+            if (x[i] > xi) {
+                xi = x[i];
+                i_ = i;
+            }
+        }
+    }
+
+    {
+        cout << "При x_ = " << x__ << endl;
+        cout << "L1= " << LagranF1(x__, xi, x[i_ + 1]) << endl;
+        cout << "L2= " << LagranF2(x__, xi, x[i_ + 1], x[i_ - 1]) << endl;
+        cout << endl;
+    }
+
+    x__ = x_[3];
+    i_ = 0;
+    xi = 0;
+
+    for (int i = 0; i < 11; ++i) {
+        if (x[i] < x__) {
+            if (x[i] > xi) {
+                xi = x[i];
+                i_ = i;
+            }
+        }
+    }
+
+    {
+        cout << "При x_ = " << x__ << endl;
+        cout << "L1= " << LagranF1(x__, xi, x[i_ + 1]) << endl;
+        cout << "L2= " << LagranF2(x__, xi, x[i_ + 1], x[i_ - 1]) << endl;
+        cout << endl;
+    }
 
     return 0;
 }
